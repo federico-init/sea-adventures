35f07e145147eee2f9e98da221b4984c7af9b327
unknown <fefe.borrometi@gmail.com>
2023-08-03 16:46:47 +0200

    chore: design system set up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    additionalData: `
+    @import ".src/styles/helpers/devices.scss";
+    @import ".src/styles/helpers/mixins.scss";
+    @import ".src/styles/variables/colors.scss";
+    @import ".src/styles/variables/typography.scss";
+    `,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+// import style
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
