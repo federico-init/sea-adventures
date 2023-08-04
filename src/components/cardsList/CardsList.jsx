87b6ce0c15c7d62ed3353e948750157bc537823e
unknown <fefe.borrometi@gmail.com>
2023-08-04 02:30:23 +0200

    feat: step2/show more button

--- src/components/cardsList/CardsList.jsx
+++ src/components/cardsList/CardsList.jsx
@@ -1,3 +1,6 @@
+// import React
+import { useState } from "react";
+
 // import component
 import Card from "@/components/card";
 
@@ -8,11 +11,23 @@ import { data } from "@/mock/data";
 import styles from "./CardsList.module.scss";
 
 const CardsList = () => {
+  const [maxCard, setMaxCard] = useState(8);
+
+  const onHandleClick = () => {
+    setMaxCard(maxCard + 8);
+  };
+
   return (
     <section className={styles.CardsList}>
-      {data.map((itinerary) => (
-        <Card data={itinerary} key={itinerary.id} />
-      ))}
+      <div className={styles.Cards}>
+        {data.map(
+          (itinerary, i) =>
+            i < maxCard && <Card data={itinerary} key={itinerary.id} />
+        )}
+      </div>
+      {maxCard !== data.length && (
+        <button onClick={onHandleClick}>Mostra altri</button>
+      )}
     </section>
   );
 };
